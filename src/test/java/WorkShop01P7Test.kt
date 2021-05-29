c609f7ea13b626c3a2dc729a07a6e994a0825178
zharkovmax <zharkovmax@gmail.com>
2021-05-30 00:59:14 +0700

    completed WorkShop01

--- src/test/java/WorkShop01P7Test.kt
+++ src/test/java/WorkShop01P7Test.kt
@@ -12,33 +12,33 @@ class WorkShop01P7Test {
     }
 
     //TODO Раскомментировать тесты и прогнать
-//    @Test
-//    fun `num1 less than num2 with lambda lessOrEqual`() {
-//        assertTrue(WorkShop01.compareTwoInts(1, 2, lessOrEqual))
-//    }
-//
-//    @Test
-//    fun `num1 equal num2 with lambda lessOrEqual`() {
-//        assertTrue(WorkShop01.compareTwoInts(1, 1, lessOrEqual))
-//    }
-//
-//    @Test
-//    fun `num1 more than num2 with lambda lessOrEqual`() {
-//        assertFalse(WorkShop01.compareTwoInts(2, 1, lessOrEqual))
-//    }
-//
-//    @Test
-//    fun `num1 less than num2 with lambda moreOrEqual`() {
-//        assertFalse(WorkShop01.compareTwoInts(1, 2, moreOrEqual))
-//    }
-//
-//    @Test
-//    fun `num1 equal num2 with lambda moreOrEqual`() {
-//        assertTrue(WorkShop01.compareTwoInts(2, 2, moreOrEqual))
-//    }
-//
-//    @Test
-//    fun `num1 more than num2 with lambda moreOrEqual`() {
-//        assertTrue(WorkShop01.compareTwoInts(3, 2, moreOrEqual))
-//    }
+    @Test
+    fun `num1 less than num2 with lambda lessOrEqual`() {
+        assertTrue(WorkShop01.compareTwoInts(1, 2, lessOrEqual))
+    }
+
+    @Test
+    fun `num1 equal num2 with lambda lessOrEqual`() {
+        assertTrue(WorkShop01.compareTwoInts(1, 1, lessOrEqual))
+    }
+
+    @Test
+    fun `num1 more than num2 with lambda lessOrEqual`() {
+        assertFalse(WorkShop01.compareTwoInts(2, 1, lessOrEqual))
+    }
+
+    @Test
+    fun `num1 less than num2 with lambda moreOrEqual`() {
+        assertFalse(WorkShop01.compareTwoInts(1, 2, moreOrEqual))
+    }
+
+    @Test
+    fun `num1 equal num2 with lambda moreOrEqual`() {
+        assertTrue(WorkShop01.compareTwoInts(2, 2, moreOrEqual))
+    }
+
+    @Test
+    fun `num1 more than num2 with lambda moreOrEqual`() {
+        assertTrue(WorkShop01.compareTwoInts(3, 2, moreOrEqual))
+    }
 }
