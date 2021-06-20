de2823d2d4ce81009446b1d35e3fb0b63b9af4d2
Sergey Potapov <itsergpot@gmail.com>
2021-06-20 22:33:13 +0300

    fixes

--- src/main/java/ws02/Vector.kt
+++ src/main/java/ws02/Vector.kt
@@ -5,97 +5,102 @@ import java.lang.IllegalArgumentException
 import kotlin.math.cos
 import kotlin.math.sqrt
 
-open class Vector(size: Int) : Comparable<Vector> {
+open class Vector(size: Int): Comparable<Vector> {
+
+    private val defaultInitValue = 0.0
+    var vectorComponentsList: MutableList<Double>
 
     // 1. Конструктор "По умолчанию". Создает вектор, у которого все компоненты содержат нули (для N-мерного вектора, конструктор "по умолчанию" должен принимать размер)
 
-
-    var vectorComponentsList = mutableListOf<Double>()
-
     init {
-        for (n in 0 until size){
-            vectorComponentsList.add(0.0)
-        }
+        this.vectorComponentsList = initVectorComponentsList(size)
     }
+
+    private fun initVectorComponentsList (size: Int, initValue: Double? = 0.0): MutableList<Double> =
+    MutableList(size) { initValue ?: defaultInitValue }
+
     // 2. Конструктор, который позволяет создать объект с указанием 3-х компонент для трехмерного и N компонент N-мерного (можно использовать vararg для N-мерного)
     constructor(vararg vectorElements: Double): this(vectorElements.size) {
-        this.vectorComponentsList = vectorElements.toMutableList()
+        vectorComponentsList = vectorElements.toMutableList()
     }
 
 
     // Реализовать операции, которые доступны с векторами:
     // 1. Опеределение длины вектора (вычисляется как квадратный корень из суммы квадратов всех компонент)
 
-    fun determineLengthOfVector(vectorComponents: List<Double>): Double = sqrt(vectorComponents.fold(0.0) { acc, current -> acc + current })
+    fun length(incVector: Vector): Double = sqrt(incVector.vectorComponentsList.fold(0.0) { acc, current -> acc + current })
 
     // 2. Сложение и вычитание двух векторов (вычисляется как сумма/разность соответствующих координат вектора)
 
-    fun vectorSubtraction(incVector: Vector) {
-        try {
-            if (this.vectorComponentsList.size != incVector.vectorComponentsList.size) {
-                throw IllegalArgumentException()
-            }
-            val resultOfOperationUnderVectorComponents = mutableListOf<Double>()
+    fun vectorSubtraction(incVector1: Vector, incVector2: Vector): Vector? {
+        return try {
+            checkSize(incVector1, incVector2)
+            val resultVector = Vector()
             for (n in 0 until this.vectorComponentsList.size) {
-                resultOfOperationUnderVectorComponents.add(this.vectorComponentsList[n] - incVector.vectorComponentsList[n])
-                this.vectorComponentsList[n] = resultOfOperationUnderVectorComponents[n]
+                resultVector.vectorComponentsList[n] = incVector1.vectorComponentsList[n].minus(incVector2.vectorComponentsList[n])
             }
-        } catch (e: Exception) {
-            println(e)
+            resultVector
+        } catch (e: IllegalArgumentException) {
+            println("Different vectors in length were passed to the input")
+            null
         }
     }
 
-    fun vectorAddition(incVector: Vector) {
-        try {
-            if (this.vectorComponentsList.size != incVector.vectorComponentsList.size) {
-                throw IllegalArgumentException()
-            }
-            val resultOfOperationUnderVectorComponents = mutableListOf<Double>()
+    fun vectorAddition(incVector1: Vector, incVector2: Vector): Vector? {
+        return try {
+            checkSize(incVector1, incVector2)
+            val resultVector = Vector()
             for (n in 0 until this.vectorComponentsList.size) {
-                resultOfOperationUnderVectorComponents.add(this.vectorComponentsList[n] + incVector.vectorComponentsList[n])
-                this.vectorComponentsList[n] = resultOfOperationUnderVectorComponents[n]
+                resultVector.vectorComponentsList[n] = incVector1.vectorComponentsList[n].plus(incVector2.vectorComponentsList[n])
             }
-        } catch (e: Exception) {
-            println(e)
+            resultVector
+        } catch (e: IllegalArgumentException) {
+            println("Different vectors in length were passed to the input")
+            null
+        }
+    }
+
+    private fun checkSize(incVector1: Vector, incVector2: Vector) {
+        if (incVector1.vectorComponentsList.size != incVector2.vectorComponentsList.size) {
+            throw IllegalArgumentException()
         }
     }
 
     // 3. Скалярное произведение двух векторов
 
-    fun scalarProductOfVectors(incVector: Vector) : Double? {
+    fun scalarProductOfVectors(incVector1: Vector, incVector2: Vector) : Double {
         return try {
-            if (this.vectorComponentsList.size != incVector.vectorComponentsList.size) {
-                throw IllegalArgumentException()
-            }
+            checkSize(incVector1, incVector2)
             var resultOfOperationUnderVectorComponents = 0.0
             for (n in 0 until this.vectorComponentsList.size) {
-                resultOfOperationUnderVectorComponents += this.vectorComponentsList[n] * incVector.vectorComponentsList[n]
+                resultOfOperationUnderVectorComponents += incVector1.vectorComponentsList[n] * incVector2.vectorComponentsList[n]
             }
             resultOfOperationUnderVectorComponents
         } catch (e: Exception) {
             println(e)
-            null
+            0.0
         }
     }
 
     // 4. Умножение вектора на число
 
-    fun multiplyingVectorByNumber(multiplier: Int) = this.vectorComponentsList.map { vectorComponent -> multiplier * vectorComponent}
+    fun multiplyingVectorByNumber(multiplier: Int, incVector: Vector): Vector {
+        incVector.vectorComponentsList.map { vectorComponent -> multiplier * vectorComponent }
+        return incVector
+    }
 
     // 5. Вычисление косинуса между двумя векторами
 
-    fun cosineOfAngleBetweenTwoVectors(incVector: Vector) : Double? {
+    fun cosOfAngleBetweenTwoVectors(incVector1: Vector, incVector2: Vector) : Double? {
         return try {
-            if (this.vectorComponentsList.size != incVector.vectorComponentsList.size) {
-                throw IllegalArgumentException()
-            }
-            if (determineLengthOfVector(this.vectorComponentsList) != 0.0 && determineLengthOfVector(incVector.vectorComponentsList) != 0.0) {
+            checkSize(incVector1, incVector2)
+            if (length(incVector1) != 0.0 && length(incVector2) != 0.0) {
                 throw ArithmeticException()
             }
-            val angleCalculationResult = cos(scalarProductOfVectors(incVector)!! / (determineLengthOfVector(this.vectorComponentsList) * determineLengthOfVector(incVector.vectorComponentsList)))
+            val angleCalculationResult = cos(scalarProductOfVectors(incVector1, incVector2) / (length(incVector1) * length(incVector2)))
             angleCalculationResult
-        } catch (e: Exception) {
-            println(e)
+        } catch (e: ArithmeticException) {
+            println("The length of one of the vectors is 0")
             null
         }
     }
@@ -112,8 +117,8 @@ open class Vector(size: Int) : Comparable<Vector> {
     // См. https://kotlinlang.org/docs/operator-overloading.html#comparison-operators
 
     override fun compareTo(other: Vector) : Int = when {
-            this.determineLengthOfVector(this.vectorComponentsList) > determineLengthOfVector(other.vectorComponentsList) -> 1
-            this.determineLengthOfVector(this.vectorComponentsList) < determineLengthOfVector(other.vectorComponentsList) -> -1
+            this.length(this) > length(other) -> 1
+            this.length(this) < length(other) -> -1
             else -> 0
     }
 
--- src/main/java/ws02/WorkShop02P01.kt
+++ src/main/java/ws02/WorkShop02P01.kt
@@ -21,12 +21,15 @@ fun Shop.getCustomersWithAtLeastOneDeliveredOrder(): List<Customer> = this.custo
 
 //TODO 1.5*. Реализовать функцию getCustomersWithTotalAmountMoreThan, возвращающая клиентов,
 // которые заказали товары суммарно по всем заказам больше(либо равно) указанной суммы
-fun Shop.getCustomersWithTotalAmountMoreThan(amount: Double): List<Customer> = this.customers.filter { customer -> customer.orders.flatMap { order -> order.products }.sumByDouble { product -> product.price } >= amount}
+fun Shop.getCustomersWithTotalAmountMoreThan(amount: Double): List<Customer> = this.customers.filter { customer -> customer.orders
+    .flatMap { order -> order.products }
+    .sumByDouble { product -> product.price } >= amount}
 
 //TODO 1.6*. Реализовать функцию,
 // которая возвращает самый популярный продукт в магазине (по количеству продукта во всех заказах всех покупателей)
 // Если продукт не найден, то необходимо вернуть null - (см. https://kotlinlang.org/docs/null-safety.html)
-fun Shop.getMostPopularProduct(): Product? = customers.flatMap { customer -> customer.orders.flatMap { order -> order.products } }
+fun Shop.getMostPopularProduct(): Product? = customers.flatMap { customer -> customer.orders
+    .flatMap { order -> order.products } }
     .groupingBy { it }
     .eachCount()
     .maxBy { it.value }?.key
