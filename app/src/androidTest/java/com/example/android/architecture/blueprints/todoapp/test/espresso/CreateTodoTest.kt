1c0fd587be3951a4d1c336a1978975f747b6b38b
Dmitriy Subbotin <d.subbotin@cft.ru>
2021-06-23 12:01:01 +0700

    fix build.gradle

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/test/espresso/CreateTodoTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/test/espresso/CreateTodoTest.kt
@@ -5,13 +5,12 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.android.architecture.blueprints.todoapp.screen.espresso.AddTodoScreen
 import com.example.android.architecture.blueprints.todoapp.screen.espresso.TodoListScreen
 import com.example.android.architecture.blueprints.todoapp.tasks.TasksActivity
-import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class CreateTodoTest : TestCase() {
+class CreateTodoTest {
 
 	@get:Rule
 	val activityScenarioRule = ActivityScenarioRule<TasksActivity>(TasksActivity::class.java)
